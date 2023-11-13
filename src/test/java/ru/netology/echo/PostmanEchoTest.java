139df934702ebd5bdb402ab0d638c84a10a698df
Aleksey <Lesha55-90@mail.ru>
2023-11-13 19:07:01 +0600

    Ready code

---
+++ src/test/java/ru/netology/echo/PostmanEchoTest.java
@@ -0,0 +1,23 @@
+package ru.netology.echo;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.jupiter.api.Test;
+
+public class PostmanEchoTest {
+    @Test
+    void shouldResponseEcho() {
+        String text = "Hi";
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Hi"))
+        ;
+    }
+
+}
